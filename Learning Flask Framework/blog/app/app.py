b4a2772bb150e4c334dd67717c23c0f8244182f1
simongarisch <gatman946@gmail.com>
2019-04-15 17:44:45 +1000

    Working with database setup and migrations

--- Learning Flask Framework/blog/app/app.py
+++ Learning Flask Framework/blog/app/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
-try:
-    from flask.ext.sqlalchemy import SQLAlchemy
-except:
-    from flask_sqlalchemy import SQLAlchemy  # this is the new way to do it which works!
+
+# https://flask-migrate.readthedocs.io/en/latest/
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager  # pip install Flask-Script
+
+from flask_sqlalchemy import SQLAlchemy  # this is the new way to do it which works!
 from config import Configuration # import our configuration data
 
 app = Flask(__name__)
 app.config.from_object(Configuration) # use values from our Configuration object.
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
