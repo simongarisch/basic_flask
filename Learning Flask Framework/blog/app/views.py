13c04ad8ef9a20bd7866b12eed65ecfe82964949
simongarisch <gatman946@gmail.com>
2019-04-15 18:35:12 +1000

    Working with Flask templates

--- Learning Flask Framework/blog/app/views.py
+++ Learning Flask Framework/blog/app/views.py
@@ -1,5 +1,11 @@
+from flask import render_template, request
 from app import app
 
 @app.route('/')
 def homepage():
-    return 'Home page'
+    name = request.args.get('name')
+    if not name:
+        name = '<unknown>'
+    # see at http://localhost:5000/?name=Simon
+    # move to Loops, control structures, and template programming
+    return render_template('homepage.html', name=name)
